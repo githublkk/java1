8314da86d175e771cb908659890e41df0cf26761
hubot <1650754224@qq.com>
2018-02-05 13:42:08 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+    public static void  dome(){
+
+        System.out.println("holle world");
+    }
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<String>();
+        list.add("你好");
+        list.add("明天");
+        Test.dome();
+        System.out.println(list);
+    }
+}
